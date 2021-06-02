fd134be89d980f009fb940d6e6d82a79f2488c95
ok97 <omkhawshi0@gmail.com>
2021-06-02 14:58:01 +0530

    Added: Ability to Read Entries of Address Book

---
+++ AddressBookSystem-RESTSharp/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace AddressBookSystem_RESTSharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        RestClient client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            //Initialize the base URL to execute requests made by the instance
+            client = new RestClient("http://localhost:3000");
+        }
+        private IRestResponse GetContactList()
+        {
+            //Arrange
+            //Initialize the request object with proper method and URL
+            RestRequest request = new RestRequest("/Contacts", Method.GET);
+            //Act
+            // Execute the request
+            RestSharp.IRestResponse response = client.Execute(request);
+            return response;
+        }
+       
+         /* UC22:- Ability to Read Entries of Address Book from JSONServer.
+                   - Use RESTSharp for REST Api Calls from MSTest Test Code.
+                   - Update Address Book Memory of the Data retrieved from JSON Server.
+         */
+       
+        [TestMethod]
+        public void ReadEntriesFromJsonServer()
+        {
+            IRestResponse response = GetContactList();
+            // Check if the status code of response equals the default code for the method requested
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            // Convert the response object to list of employees
+            List<Contact> employeeList = JsonConvert.DeserializeObject<List<Contact>>(response.Content);
+            Assert.AreEqual(2, employeeList.Count);
+            foreach (Contact c in employeeList)
+            {
+                Console.WriteLine($"Id: {c.Id}\tFullName: {c.FirstName} {c.LastName}\tPhoneNo: {c.PhoneNumber}\tAddress: {c.Address}\tCity: {c.City}\tState: {c.State}\tZip: {c.Zip}\tEmail: {c.Email}");
+            }
+        }
+    }
+}
